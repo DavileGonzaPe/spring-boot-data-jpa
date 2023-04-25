ea1b0271a12e3897ca2d01a58079ecae77626f18
Davile <davidgonzalezperez81@gmail.com>
2023-04-25 09:36:22 +0200

    Creando controlador Cliente con la acción handler listar

--- src/main/java/com/davile/springboot/app/models/dao/ClienteDaoImpl.java
+++ src/main/java/com/davile/springboot/app/models/dao/ClienteDaoImpl.java
@@ -10,7 +10,7 @@ import com.davile.springboot.app.models.entity.Cliente;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
-@Repository
+@Repository("clienteDaoJPA")
 public class ClienteDaoImpl implements IClienteDao {
 	
 	@PersistenceContext
